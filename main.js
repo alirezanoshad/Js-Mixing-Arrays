378584eaa70416bd9a97fccd925ee8d8ec3519c8
Alireza <iranexplosm@gmail.com>
2024-08-11 16:50:58 +0330

    files added

---
+++ main.js
@@ -0,0 +1,8 @@
+//the function base code.
+let loz =[8,7,6];
+let poz =[4,2,9];
+function matchMaker(fArray, sArray) {
+    return fArray.concat(sArray)
+  }
+  //testing.
+  console.log(matchMaker(loz,poz));
